a8c9bac5e75152ce3e28cf136b25bb7bd2a7d98a
bengin34 <becaglar3434@gmail.com>
2023-05-26 18:01:23 +0300

    proposal

--- 001/Proposal_Contract.sol
+++ 001/Proposal_Contract.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.18;
 
 contract ProposalContract {
+    //Data
+
+
    //Owner 
     address owner;
 
+
+    //Proposal Structure
     struct Proposal {
         string description;
         uint256 approve;
@@ -14,4 +19,29 @@ contract ProposalContract {
         bool current_state;
         bool is_active;
     }
+    //mapping (for keeping the proposal history )
+    mapping(uint256 => Proposal) proposal_history;
+
+    //array, for keeping voters
+    address[] private voted_addresses;
+
+    constructor() {
+        owner = msg.sender;
+        voted_addresses.push(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+
+
+    //Execute Functions 
+    function setOwner(address new_owner) external onlyOwner {
+        owner = new_owner;
+    }
+
+    //Query Functions
+
 }
